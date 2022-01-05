efc2a9e77718519c80f3261e97227e85de00101f
KOMLANI Tibang Tibang'bé <55633949+tibangk01@users.noreply.github.com>
2022-01-05 13:31:18 +0000

    Add crud view template
    These templates will be used like skeleton for
    application crud operations

---
+++ application/View/_templates/crud_template/index.php
@@ -0,0 +1,49 @@
+<!-- change 'book' with the crud entity -->
+<div class="right_col">
+
+    <div class="row">
+
+        <div class="x_panel">
+
+            <div class="x_content">
+
+                <div class="row">
+                    <div class="col-sm-12 text-left p-3 m-2">
+                        <a href="<?php echo URL ?>book/create" class="btn btn-sm btn-primary">
+                            <i class="fa fa-plus" aria-hidden="true"></i> Add New book
+                        </a>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-12">
+
+                        <table class="table table-bordered datatable">
+                            <thead>
+                                <tr>
+                                    <th>Title</th>
+                                    <th class="text-center">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Le Monde s'éffondre</td>
+                                    <td class="text-center">
+                                        <a href="<?php echo URL . 'book/show/1'  ?>" class="btn btn-primary btn-sm btn-xs"><i class="fa fa-eye"></i> View </a>
+                                        <a href="<?php echo URL . 'book/edit/1'  ?>" class="btn btn-info btn-sm btn-xs"><i class="fa fa-pencil"></i> Edit </a>
+                                        <a href="<?php echo URL . 'book/delete/1' ?>" class="btn btn-danger btn-sm btn-xs"><i class="fa fa-trash-o"></i> Delete </a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
---
+++ application/View/_templates/crud_template/show.php
@@ -0,0 +1,50 @@
+<div class="right_col">
+
+    <div class="row">
+
+        <div class="x_panel">
+
+            <div class="x_content">
+
+                <div class="row">
+                    <div class="col-sm-12 text-left p-3 m-2">
+                        <!-- concanate entity id for edit & delete eg: $book->id -->
+                        <a href="<?php echo URL . 'book/edit/'  ?>" class="btn btn-sm btn-primary">
+                            <i class="fa fa-pencil" aria-hidden="true"></i> Edit
+                        </a>
+                        <a href="<?php echo URL . 'book/delete/'?>" class="btn btn-sm btn-primary">
+                            <i class="fa fa-trash" aria-hidden="true"></i> Delete
+                        </a>
+                        <a href="<?php echo URL ?>book" class="btn btn-sm btn-primary">
+                            <i class="fa fa-list" aria-hidden="true"></i> Return To List
+                        </a>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-12">
+
+                        <table class="table table-bordered">
+
+                            <tbody>
+
+                                <tr>
+                                    <td>Title</td>
+                                    <td>Mary</td>
+                                </tr>
+                      
+                            </tbody>
+
+                        </table>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
