0e0e2d34e11d1380f9b1cad52dc77edd30f28cfb
KOMLANI Tibang Tibang'bé <55633949+tibangk01@users.noreply.github.com>
2022-01-06 08:48:48 +0000

    Create BookModel.php
    Add book model methods to operate crud operations

---
+++ application/Model/BookModel.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Auth7\Model;
+
+use Auth7\Core\Model;
+
+class BookModel extends Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function getAll(): array
+    {
+        $sql = "SELECT 
+                    id, 
+                    title 
+                FROM 
+                    books";
+        $query = $this->db->prepare($sql);
+        $query->execute();
+
+        return $query->fetchAll();
+    }
+
+    public function get(int $bookId): mixed
+    {
+        $sql = "SELECT 
+                   id, 
+                   title 
+                FROM books 
+                WHERE id = :bookId";
+
+        $query = $this->db->prepare($sql);
+        $parameters = [':bookId' => $bookId];
+
+        $query->execute($parameters);
+
+        return ($query->rowcount() ? $query->fetch() : false);
+    }
+
+    public function store(array $data): bool
+    {
+        $sql = "INSERT INTO
+                    books(title, registered, added_by)
+                VALUES
+                (:title, :registered, :added_by)";
+
+        $query = $this->db->prepare($sql);
+        $parameters = [
+            ':title' => $data['title'],
+            ':registered' => time(),
+            'added_by' => $_SESSION['auth7_userId'],
+        ];
+
+        return $query->execute($parameters);
+    }
+
+    public function update(int $bookId, array $data): bool
+    {
+        $sql = "UPDATE
+                    books
+                SET
+                    title = :title
+                where
+                    id = :id";
+        $query = $this->db->prepare($sql);
+        $parameters = [
+            ':title' => $data['title'],
+            ':id' => $bookId,
+        ];
+
+        return $query->execute($parameters);
+    }
+
+    public function destroy(int $bookId): bool
+    {
+        $sql = "DELETE FROM books WHERE id = :id";
+        $query = $this->db->prepare($sql);
+
+        $parameters = [':id' => $bookId];
+
+        return $query->execute($parameters);
+    }
+}
